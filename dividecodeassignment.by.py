9e20b34b1f7389240d029f9a92a54e913a6f696a
EMIKO548 <85025213+EMIKO548@users.noreply.github.com>
2021-06-09 20:43:45 +0100

    Add files via upload

---
+++ dividecodeassignment.by.py
@@ -0,0 +1,14 @@
+# This is a small python program to divide a numerator by a denominator.  Division means repeated substraction.
+# Division will occur as along as the denominator is not 0. No numerator can be divided by 0 to give an any positive or 
+# negative integer as it is epistemologically impossible. The answer is undefined in a mathematical context.
+
+num1=1
+num2=2
+
+div=num1/num2;
+print("The division of {0} and {1} is: {2}".format(num1,num2,div))
+
+
+
+
+
